a36660b986766713a15ad430b129339de2d151c1
Sophie Shu <sophieshu3@gmail.com>
2026-03-28 20:07:47 -0700

    fix progress bar not displaying correct values

--- www/src/components/progress-bar.tsx
+++ www/src/components/progress-bar.tsx
@@ -3,10 +3,10 @@ export default function ProgressBar({ top, bottom }: { top: number; bottom: numb
     <div className="w-full">
       <div className="flex h-2 rounded-full overflow-hidden divide-x divide-border-light">
         <div className="flex-1 bg-border-normal overflow-hidden">
-          <div className={`w-[${bottom}%] h-full bg-[#86dee472] rounded-l-full float-right`}></div>
+          <div className="h-full bg-[#86dee472] rounded-l-full float-right" style={{ width: `${bottom}%` }}></div>
         </div>
         <div className="flex-1 bg-border-normal overflow-hidden">
-          <div className={`w-[${top}%] h-full bg-[#e4869377] rounded-r-full`}></div>
+          <div className="h-full bg-[#e4869377] rounded-r-full" style={{ width: `${top}%` }}></div>
         </div>
       </div>
       <div className="mt-1 flex justify-between">
